23de2a092f8e683a337d9dd3984a1bda7c6a9b13
David <juliuming@163.com>
2025-09-25 16:22:45 +0800

    【修改】删除旧版应付

--- src/json/router.ts
+++ src/json/router.ts
@@ -1,53 +1,5 @@
 export default [
   {
-    name: 'home',
-    path: '/home',
-    component: 'basic',
-    meta: {
-      title: '应付管理',
-      localIcon: 'circum_crop',
-      order: 1
-    },
-    children: [
-      {
-        name: 'home_LadingBillhome',
-        path: '/home/LadingBillhome',
-        component: 'self',
-        meta: {
-          title: '应付帐单',
-          localIcon: 'dot',
-          requiresAuth: true,
-          hide: false,
-          keepAlive: true
-        }
-      },
-      {
-        name: 'home_edit',
-        path: '/home/edit',
-        component: 'self',
-        meta: {
-          title: '对账详情',
-          localIcon: 'dot',
-          requiresAuth: true,
-          hide: true,
-          keepAlive: false
-        }
-      },
-      {
-        name: 'home_message',
-        path: '/home/message',
-        component: 'self',
-        meta: {
-          title: '服务费列表',
-          localIcon: 'dot',
-          requiresAuth: false,
-          hide: false,
-          keepAlive: true
-        }
-      }
-    ]
-  },
-  {
     name: 'payable_statement',
     path: '/payable_statement',
     component: 'basic',
