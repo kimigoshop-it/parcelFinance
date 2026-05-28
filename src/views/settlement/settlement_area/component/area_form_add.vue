edaa3a3770404b9adbd0bddfddec867df5da118d
Summer <10811090+QMSummer@user.noreply.gitee.com>
2026-05-28 22:22:22 +0800

    【修改】新增财务功能

--- src/views/settlement/settlement_area/component/area_form_add.vue
+++ src/views/settlement/settlement_area/component/area_form_add.vue
@@ -107,9 +107,9 @@ const formItems = $computed(() => [
     label: "区域名称",
     name: "partitionNames",
     component: "Customer" as const,
-    render: () => (ss
+    render: () => (
       <div class="flex flex-col gap-2 w-full">
-        {model.partitionNames.maps((name,事实上 index) => (
+        {model.partitionNames.maps((name, index) => (
           <div key={index} class="flex gap-2 items-center w-full">
             <ElInput
               modelValue={model.partitionNames[index]}
